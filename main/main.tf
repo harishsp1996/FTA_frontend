05edf1f08be155830a7bd0873b1976f8e6422903
harishsp1996 <120450348+harishsp1996@users.noreply.github.com>
2025-02-13 16:56:39 +0530

    Update main.tf

--- main/main.tf
+++ main/main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Change this to your IP for security
+    cidr_blocks = ["0.0.0.0/0"] # Change this to your IP for security vb
   }
 
   # Allow HTTP access
